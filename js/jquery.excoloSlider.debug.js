7cacd3be65c0996c44a5133cf3d6f60ada914ff6
Nikolaj Dam Larsen <nikolaj@impero.dk>
2013-05-21 18:54:25 +0200

    changed version to fit tag

--- js/jquery.excoloSlider.debug.js
+++ js/jquery.excoloSlider.debug.js
@@ -5,13 +5,13 @@
  * http://excolo.github.io/Excolo-Slider/
  *
  * Author: Nikolaj Dam Larsen
- * Version: 0.1.3 (19-MAY-2013)
+ * Version: 0.1.4 (19-MAY-2013)
  *
  * Released under the MIT license
  * https://github.com/Excolo/ExcoloSlider/blob/master/MIT-License.txt
  */
 ;(function ($, window, document, undefined) {
-    var version = "0.1.3";
+    var version = "0.1.4";
     var pluginName = "excoloSlider";
 
 
